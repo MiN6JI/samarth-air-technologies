d7ea10e9224c0ab3477bcf62855fc33d9b6823da
MINGJI <deorukhkarshruti232@gmail.com>
2026-07-27 16:52:36 +0530

    Changes

--- src/pages/Services/Maintenance.tsx
+++ src/pages/Services/Maintenance.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   LuWrench as Wrench,
   LuGauge as GaugeIcon,
--- src/pages/Services/Residential.tsx
+++ src/pages/Services/Residential.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   LuSun as Sun,
   LuHouse as Home,
