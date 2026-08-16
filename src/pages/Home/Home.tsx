e46c76603df48567a994904875e354c3db6910c5
MINGJI <deorukhkarshruti232@gmail.com>
2026-08-17 02:34:41 +0530

    Removed unnecessary imports from home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,8 @@ import About from "./Sections/About";
 import StatsBar from "./Sections/StatsBar";
 import Cta from "./Sections/Cta";
 
-import Calculator from "./Sections/Calculator";
-import Comparison from "./Sections/comparision";
-import MonitoringApp from "./Sections/MonitoringApp";
-import TrueSection from "./Sections/TrustSection";
 import Faq from "./Sections/Faq";
-import GoodZeroBanner from "./Sections/GoodZero";
-import Blogs from "./Sections/Blogs";
+
 const Home = () => {
   return (
     <>
