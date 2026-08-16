9a5fa276057a948f4b5f9802ca053d4d15cf4eb8
MINGJI <deorukhkarshruti232@gmail.com>
2026-08-17 02:38:52 +0530

    Removed unecessary things

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,5 @@
 import PageHeader from "../../components/PageHeader/PageHeader";
-import Services from "./Sections/Services";
 import WhatWeWork from "./Sections/WhatWeWork";
-import WhatWeDo from "./Sections/WhatWeDo";
-import Advantage from "./Sections/Advantage";
 import Cta from "../Home/Sections/Cta";
 const About = () => {
   return (
@@ -13,9 +10,6 @@ const About = () => {
         breadcrumbs={[{ label: "About Us", href: "/about" }]}
       />
       <WhatWeWork />
-      {/* <Services /> */}
-      {/* <WhatWeDo /> */}
-      {/* <Advantage /> */}
       <Cta
         backgroundImage={
           "https://demo.awaikenthemes.com/coolify/demo2/wp-content/uploads/2025/07/what-we-do-image.jpg"
